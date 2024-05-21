c54aa7906b4b5e8fdff4571afade772828b5fcec
Ademir Alijagic <115159286+Ade-mir@users.noreply.github.com>
2024-05-21 18:19:28 +0200

    Fix checkmark icons not showing on light mode

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,8 +1,13 @@
 import styles from './SkillsStyles.module.css';
-import checkMarkIcon from '../../assets/checkmark-dark.svg';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
 import SkillList from '../../common/SkillList';
+import { useTheme } from '../../common/ThemeContext';
 
 function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
